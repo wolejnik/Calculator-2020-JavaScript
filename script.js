bba5f3f409b1e93df713c9ccf7a1df2ebe45c0df
wolejnik <olejnik.wojciech1@gmail.com>
2019-12-27 14:23:43 +0100

    Fixed bugs.

--- script.js
+++ script.js
@@ -1,108 +1,130 @@
 ((document) => { //not poluting globals
-	
-class Calculator {
-    constructor(lastValueInput, currentValueInput) {
-      this.currentValue = "";
-      this.lastValue = "";
+
+  class Calculator {
+  constructor(lastValueInput, currentValueInput) {
+      this.currentValue = '';
+      this.lastValue = '';
       this.operation = undefined;
-      this.result = "";
+      this.result = '';
       this.lastValueInput = lastValueInput;
       this.currentValueInput = currentValueInput;
-    }
-  
-    addDigit(digit) {
-      this.currentValue += digit;
-    }
-  
-    addDot() {
-      if (this.currentValue.includes(".")) {
-        return;
-      }
-      this.currentValue += ".";
-    }
-  
-    deleteLast() {
-      this.currentValue = this.currentValue.toString().slice(0, -1);
-      this.currentValueInput.innerText = this.currentValue;
-    }
-  
-    deletaAll() {
-      this.currentValue = "";
-      this.lastValue = "";
-      this.result = "";
-      this.currentValueInput.innerText = "";
-    }
+      this.equalsPressed = false;
+  }
 
-    updateDisplay() {
-      this.currentValueInput.innerText = this.currentValue;
-    }
-  
-    setOperation(operation) {
-      this.operation = operation;
-      this.lastValue = this.currentValue;
-      this.currentValue = "";
-    }
-  
-    _calculateByOperation(operation, a, b) {
-      if (operation === "+") {
-        return a + b;
-      } else if (operation === "-") {
-        return a - b;
-      } else if (operation === "*") {
-        return a * b;
-      } else if (operation === "÷") {
-        if(b === 0) {
-          return 0;
-        }
-        return a / b;
-      } else {
-        console.error(`Unknown operation ${operation}`);
+  addDigit(digit) {
+      this.currentValue += digit;
+  }
+
+  addDot() {
+      if (this.currentValue.toString().includes('.')) {
+          return;
       }
-    }
-  
-    calculate() {
+      this.currentValue += '.';
+  }
+
+  deleteLast() {
+      if (this.equalsPressed) {
+          this.deleteAll()
+      } else {
+          this.currentValue = this.currentValue.toString().slice(0, -1);
+          this.currentValueInput.innerText = this.currentValue;
+      }
+  }
+
+  deleteAll() {
+      this.currentValue = '';
+      this.lastValue = '';
+      this.result = '';
+      this.currentValueInput.innerText = '';
+      this.equalsPressed = false;
+  }
+
+  _updateDisplayWith(value) {
+      this.currentValueInput.innerText = '' + value;
+  }
+
+  updateDisplay() {
+      this._updateDisplayWith(this.currentValue);
+  }
+
+  setOperation(operation) {
+      if (this.currentValue.length === 0) {
+          this.operation = operation;
+          return;
+      }
+      this.operation = operation;
+      if (!this.equalsPressed) {
+          this.lastValue = this.currentValue;
+      }
+      this.equalsPressed = false;
+      this.currentValue = '';
+  }
+
+  _calculateByOperation(operation, a, b) {
+      if (operation === '+') {
+          return a + b;
+      } else if (operation === '-') {
+          return a - b;
+      } else if (operation === '*') {
+          return a * b;
+      } else if (operation === '÷') {
+          if(b === 0) {
+              return 0;
+          }
+          return a / b;
+      } else {
+          console.error(`Unknown operation ${operation}`);
+      }
+  }
+
+  calculate() {
+      this.equalsPressed = true;
       const a = parseFloat(this.lastValue);
       const b = parseFloat(this.currentValue);
       if (isNaN(a) || isNaN(b)) {
-        return;
+          return;
       }
       this.result = this._calculateByOperation(this.operation, a, b);
-      this.currentValue = this.result;
-    }
+      this.lastValue = this.result;
+      this.currentValue = this.currentValue;
+      this._updateDisplayWith(this.result);
   }
-  
-  
-  
-  const initCalcListeners = (calc) => {
-	 
-  	const initClickListenersForButtonsArray = (arrayOfButtons, buttonCallback) => {
-		arrayOfButtons.forEach(button => {
-			button.addEventListener('click', () => {
-				buttonCallback(button.innerText);
-				calc.updateDisplay();
-			});
-		});
-	};
-	  
-	const digitalButtons = [...document.getElementsByClassName('btn-primary')];
-	const dotButton = [...document.getElementsByClassName('btn-secondary')];
-	const equalsButton = [...document.getElementsByClassName('btn-success')];
-	const operationsButton = [...document.getElementsByClassName('btn-info')];
-	const deleteAllButton = document.querySelector('.delete-all');
-	const deleteButton  = document.querySelector('.delete-one');
-	  
-	initClickListenersForButtonsArray(digitalButtons, text => calc.addDigit(text));
-	initClickListenersForButtonsArray(operationsButton, text => calc.setOperation(text));
-	initClickListenersForButtonsArray(dotButton, () => calc.addDot());
-	initClickListenersForButtonsArray(equalsButton, () => calc.calculate());
-	deleteAllButton.addEventListener('click', () => calc.deletaAll());
-	deleteButton.addEventListener('click', () => calc.deleteLast());
+}
+
+
+
+const initCalcListeners = (calc) => {
+  const initClickListenersForButtonsArray = (arrayOfButtons, buttonCallback) => {
+      arrayOfButtons.forEach(button => {
+          button.addEventListener('click', () => {
+              buttonCallback(button.innerText);
+              if(!button.classList.contains('btn-info')) {
+              calc.updateDisplay();
+          }
+       });
+      });
   };
-  
-  const lastValueInput = document.querySelector('.last-operand');
-  const currentValueInput = document.querySelector('.current-operand');
-  const calc = new Calculator(lastValueInput, currentValueInput);
-  initCalcListeners(calc);
-  
+
+  const digitalButtons = [...document.getElementsByClassName('btn-primary')];
+  const dotButton = [...document.getElementsByClassName('btn-secondary')];
+  const operationsButton = [...document.getElementsByClassName('btn-info')];
+  const equalsButton = document.querySelector('.btn-success');
+  const deleteAllButton = document.querySelector('.delete-all');
+  const deleteButton  = document.querySelector('.delete-one');
+
+  initClickListenersForButtonsArray(digitalButtons, text => calc.addDigit(text));
+  initClickListenersForButtonsArray(operationsButton, text => calc.setOperation(text));
+  initClickListenersForButtonsArray(dotButton, () => calc.addDot());
+  equalsButton.addEventListener('click', () => calc.calculate());
+  deleteAllButton.addEventListener('click', () => calc.deleteAll());
+  deleteButton.addEventListener('click', () => calc.deleteLast());
+};
+
+const lastValueInput = document.querySelector('.last-operand');
+const currentValueInput = document.querySelector('.current-operand');
+const calc = new Calculator(lastValueInput, currentValueInput);
+initCalcListeners(calc);
+
+
 })(document);
-  
+
