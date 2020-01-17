454b06ea9d026dc8f7a2fa4b45af7be6bf1941b2
wolejnik <olejnik.wojciech1@gmail.com>
2020-01-17 10:29:59 +0100

    Task 2.Added calculation of the expressions.

--- script.js
+++ script.js
@@ -1,100 +1,234 @@
 ((document) => {
 
+	const isNumber = x =>  /^-?[0-9]+(\.[0-9]+)?$/.test(x)
+
+	const Operators = {
+		'^': 3,
+		'√': 3,
+		'÷': 2,
+		'*': 2,
+		'+': 1,
+		'-': 1,
+		'(': 0,
+		')': 0,
+	};
+
+	class Stack extends Array {
+		peek() {
+			return this[this.length -1];
+		}
+
+		isEmpty() {
+			return this.length === 0;
+		}
+	}
+
   class Calculator {
   constructor(lastValueInput, currentValueInput) {
-    this.currentValue = '';
-    this.lastValue = '';
-    this.operation = undefined;
-    this.result = '';
-    this.lastValueInput = lastValueInput;
-    this.currentValueInput = currentValueInput;
-    this.equalsPressed = false;
+		this.currentValue = '';
+		this.lastValue = '';
+		this.operation = undefined;
+		this.result = '';
+		this.lastValueInput = lastValueInput;
+		this.currentValueInput = currentValueInput;
+		this.equalsPressed = false;
+		this.expressionToCalculate = '';
   }
 
   addDigit(digit) {
-    this.currentValue += digit;
+		this.currentValue += digit;
+		this.expressionToCalculate += ' ' + digit;
   }
 
   addDot() {
-    if (this.currentValue.toString().includes('.')) {
-        return;
-    }
-    this.currentValue += '.';
+		if (this.currentValue.toString().includes('.')) {
+			return;
+		}
+		this.currentValue += '.';
+		this.expressionToCalculate += '.';
   }
 
   deleteLast() {
-    if (this.equalsPressed) {
-      this.deleteAll()
-    } else {
-      this.currentValue = this.currentValue.toString().slice(0, -1);
+		if (this.equalsPressed) {
+			this.deleteAll()
+		} else {
+			this.currentValue = this.currentValue.toString().slice(0, -1);
+			this.expressionToCalculate = this.expressionToCalculate.toString().slice(0, -2)
       this.currentValueInput.innerText = this.currentValue;
-    }
+      calc.updateDisplay();
+		}
   }
 
   deleteAll() {
-    this.currentValue = '';
-    this.lastValue = '';
-    this.result = '';
-    this.currentValueInput.innerText = '';
-    this.equalsPressed = false;
+		this.currentValue = '';
+		this.lastValue = '';
+		this.result = '';
+		this.currentValueInput.innerText = '';
+		this.equalsPressed = false;
+		this.expressionToCalculate = '';
   }
 
   _updateDisplayWith(value) {
-    this.currentValueInput.innerText = '' + value;
+		this.currentValueInput.innerText = '' + value;
   }
 
   updateDisplay() {
-    this._updateDisplayWith(this.currentValue);
+		this._updateDisplayWith(this.expressionToCalculate);
   }
 
   setOperation(operation) {
-    if (this.currentValue.length === 0) {
-      this.operation = operation;
-      return;
-    }
-    this.operation = operation;
-    if (!this.equalsPressed) {
-      this.lastValue = this.currentValue;
-    }
-    this.equalsPressed = false;
-    this.currentValue = '';
+		if (this.currentValue.length === 0) {
+			this.operation = operation;
+			this.expressionToCalculate += " " + this.operation;
+			return;
+		}
+		this.operation = operation;
+		this.expressionToCalculate += " " + this.operation;
+		if (!this.equalsPressed) {
+			this.lastValue = this.currentValue;
+		}
+		this.equalsPressed = false;
+		this.currentValue = '';
   }
 
   _calculateByOperation(operation, a, b) {
-    if (operation === '+') {
-      return a + b;
-    } else if (operation === '-') {
-      return a - b;
-    } else if (operation === '*') {
-      return a * b;
-    } else if (operation === '÷') {
-        if(b === 0) {
-          return 0;
-        }
-        return a / b;
-    } else if (operation === '^') {
-        if(b === 0) {
-          return 1;
-        }
-      return Math.pow(a, b);     
-    } else if (operation === '√') {
-      return Math.pow(b, 1/a);     
-    } else {
-      console.error(`Unknown operation ${operation}`);
-    }
+      if (operation === '+') {
+        return a + b;
+      } else if (operation === '-') {
+          return a - b;
+      } else if (operation === '*') {
+          return a * b;
+      } else if (operation === '÷') {
+          if(b === 0) {
+              return 0;
+          }
+          return a / b;
+      } else if (operation === '^') {
+          if(b === 0) {
+              return 1;
+          }
+          return Math.pow(a, b);     
+      } else if (operation === '√') {
+          return Math.pow(a, 1/b);     
+      } else {
+          console.error(`Unknown operation ${operation}`);
+      }
   }
 
-  calculate() {
-      this.equalsPressed = true;
-      const a = parseFloat(this.lastValue);
-      const b = parseFloat(this.currentValue);
-      if (isNaN(a) || isNaN(b)) {
-          return;
+  rpn(expression2) {
+    let resultStack = [];
+    let expression = expression2.split(" ");
+
+    for (let i = 0; i < expression.length; i++) {
+      if (!isNaN(parseFloat(expression[i]))) {
+        resultStack.push(parseFloat(expression[i]));
+      } else {
+        let v1 = parseFloat(resultStack.pop());
+        let v2 = parseFloat(resultStack.pop());
+
+        if (expression[i] === "+") {
+        resultStack.push(v1 + v2);
+        } else if (expression[i] === "-") {
+        resultStack.push(v2 - v1);
+        } else if (expression[i] === "*") {
+        resultStack.push(v1 * v2);
+        } else if (expression[i] === "÷") {
+        resultStack.push(v2 / v1);
+        } else if (expression[i] === "^") {
+        resultStack.push(Math.pow(v2, v1));
+        } else if (expression[i] === "√") {
+        resultStack.push(Math.pow(v1, 1/v2));
+        }
       }
-      this.result = this._calculateByOperation(this.operation, a, b);
-      this.lastValue = this.result;
-      this.currentValue = this.currentValue;
-      this._updateDisplayWith(this.result);
+    }
+
+    if (resultStack.length > 1) {
+      return "error";
+    } else {
+      return resultStack.pop();
+    }
+}
+
+
+  fromInfixToPrefix(infixNotationTokens) {
+    const infix = []
+    const S = new Stack();
+
+    for (const token of infixNotationTokens) {
+      if ('(' === token) {
+        S.push(token);
+        continue;
+      }
+
+      if (")" === token) {
+        while ("(" !== S.peek()) {
+          infix.push(S.pop());
+        }
+        S.pop();
+        continue;
+      }
+
+      if (Operators[token]) {
+        while (!S.isEmpty() && Operators[token] <= Operators[S.peek()]) {
+          infix.push(S.pop());
+        }
+        S.push(token);
+        continue;
+      }
+
+      if (isNumber(token)) {
+        infix.push(token);
+        continue;
+      }
+
+      throw "Error";
+    }
+    
+    while (!S.isEmpty()) {
+      infix.push(S.pop());
+    }
+
+    return infix.join(' ');
+  }
+
+tokenize(expression) {
+  const result = [];
+  let currentDigit = '';
+  const addCurrentDigit = () => { 
+    if (currentDigit !== '') {
+      if (!isNumber(currentDigit)) {
+        throw ('Invalid Digit ' + currentDigit );
+      }
+      result.push(currentDigit); 
+      currentDigit = '';
+    }
+  }
+  
+  const expresionWithoutSpaces = expression.replace(/\s+/g, "");
+  for (const token of expresionWithoutSpaces) {
+    if (/[0-9]|\./.test(token)) { 
+      currentDigit += token;
+    } else if (Operators[token] !== undefined) { 
+      addCurrentDigit();
+      result.push(token);
+    } else {
+      throw 'Invalid token ' + token;
+    }
+  }
+  addCurrentDigit();
+  return result;
+}
+
+  calculate() {
+    this.equalsPressed = true;
+    const inputTokenized = this.tokenize(this.expressionToCalculate)
+    const rpnExpression = this.fromInfixToPrefix(inputTokenized)
+    this.result = this.rpn(rpnExpression);
+    this.lastValue = this.result;
+    this.currentValue = this.currentValue;
+    this.expressionToCalculate = '';
+    this.expressionToCalculate += ' ' + this.result;
+    this._updateDisplayWith(this.result);
   }
 }
 
@@ -102,15 +236,12 @@
 
 const initCalcListeners = (calc) => {
   const initClickListenersForButtonsArray = (arrayOfButtons, buttonCallback) => {
-      arrayOfButtons.forEach(button => {
-          button.addEventListener('click', () => {
-              buttonCallback(button.innerText);
-          // Podczas wybierania operatora znikają liczby na wyświetlaczu
-          if(!button.classList.contains('btn-info')) {
-              calc.updateDisplay();
-          }
-       });
-      });
+		arrayOfButtons.forEach(button => {
+			button.addEventListener('click', () => {
+				buttonCallback(button.innerText);
+				calc.updateDisplay();
+			});
+		});
   };
 
   const digitalButtons = [...document.getElementsByClassName('btn-primary')];
@@ -135,3 +266,9 @@ initCalcListeners(calc);
 
 
 })(document);
+
+/**
+RPN("2 2 2 * +") // 2 + (2 * 2) = 6
+RPN("7 3 + 5 2 - 2 ^ *"); // (7 + 3) * (5 - 2) ^ 2 + 2√90 = 99.48683
+RPN("1 2 3 4 + * 2 / 1 - +"); // 1 + 2 * (3 + 4) / 2 - 1 = 7
+ */
